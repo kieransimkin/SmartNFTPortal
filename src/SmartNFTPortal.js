810f892cc16b3945220b6a53011ef8ac485d0dfd
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-06-12 19:41:33 +0100

    wrong place

--- src/SmartNFTPortal.js
+++ src/SmartNFTPortal.js
@@ -579,7 +579,7 @@ const getPortalAPIScripts = (smartImports, metadata, props) => {
                 parent.postMessage({request:'touchCancel',event:JSON.stringify(e)},'*');
             });
             `:''}
-            ${onScroll ? `window.addEventListener('scroll',(e) => { 
+            ${onScroll ? `document.addEventListener('scroll',(e) => { 
                 parent.postMessage({request:'scroll',event:JSON.stringify(e)},'*');
             });
             `:''}
